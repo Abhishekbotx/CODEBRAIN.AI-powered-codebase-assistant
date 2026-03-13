23407275149d5d83fe08c8a45d2f530ff0393d65
Abhishekbotx <abhishekbotx@gmail.com>
2026-03-13 12:50:47 +0530

    feat(chunker):  chunk_nodes code logic case for partial chunk out of limit addition

--- app/ingestion/chunker.py
+++ app/ingestion/chunker.py
@@ -26,7 +26,24 @@ def chunk_nodes(node,code:bytes,max_token,filepath:str):
             continue;
         
         
+        #case 2: if 2 out of 4 tokens was in token limit
         
+        if(node_token+current_tokens>max_token):
+                
+            if current_chunks:
+                chunks.append(build_chunks(current_chunks,code,filepath))
+                current_chunks=[]
+                current_tokens=0;
+            
+            current_chunks=[child]
+            current_tokens+=node_token
+            
+        else:
+            # here in this case if loop ended there are still some tokens in current_chunks which 
+            # are not added to chunks because the token limit was not breached, so we add them to chunks here
+            # it should hold nodes not dict
+            current_chunks.append(child)
+            current_tokens+=node_token
     
     #flush the remaining chunks which were in the current_chunks but not added to chunks because the token limit was not breached
     if current_chunks:
