44f40ac58027058a9a2642196724b550399917dc
Abhishekbotx <abhishekbotx@gmail.com>
2026-03-19 10:41:52 +0530

    feat(db/collection): get collection function and sparsevector embedding

--- app/db/collection.py
+++ app/db/collection.py
@@ -6,3 +6,21 @@ from app.db.client import client
 from app.db.schema import schema
 from chromadb import  SparseVectorIndexConfig, K
 
+dense_embed_fn = DefaultEmbeddingFunction()
+sparse_embed_fn = ChromaCloudSpladeEmbeddingFunction()
+
+
+def get_collection():
+    
+    
+    """
+    Call ONCE to create collection with sparse index.
+    After this, re-run scanner.py to re-ingest all chunks.
+    """
+    schema.create_index(
+        config=SparseVectorIndexConfig(
+            source_key=K.DOCUMENT,
+            embedding_function=sparse_embed_fn,
+        ),
+        key="sparse_embedding",
+    )
