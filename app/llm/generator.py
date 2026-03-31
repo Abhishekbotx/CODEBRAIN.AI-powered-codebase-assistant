64bdca7ef3ea8f9bc2acb339e33c8a2152e3b119
Abhishekbotx <abhishekbotx@gmail.com>
2026-03-31 11:18:14 +0530

    feat(llm/generator):ask function creation and necessary imports initialization

---
+++ app/llm/generator.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from langchain.messages import AIMessage, HumanMessage
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+import argparse
+from app.llm.chain import rag_chain,make_chat_chain
+
+
+#🍁Till now we are not printing content on clientside, we can need the content aswell if we want to print it in the client side"
+
+
+def ask(question: str, stream: bool = False) -> str:
+    """
+    Single-turn question answering.
+
+    Args:
+        question: natural language question about the codebase
+        stream:   if True, prints tokens as they arrive and returns full string
+
+    Returns:
+        answer string
+    """
+    
