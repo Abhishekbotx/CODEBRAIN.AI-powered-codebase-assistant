bdc759c27e7e3c4c143b27757d8f8f04c43a4c18
Abhishekbotx <abhishekbotx@gmail.com>
2026-04-14 09:12:04 +0530

    feat(sidebar/extensions): a utitlity object to have supported files

---
+++ client/src/types/types.ts
@@ -0,0 +1,21 @@
+
+export const SUPPORTED_EXTENSIONS = [
+  '.py',
+  '.ts',
+  '.js',
+  '.tsx',
+  '.jsx',
+  '.java',
+  '.go',
+  '.rs',
+  '.cpp',
+  '.c',
+  '.h',
+  '.cs',
+  '.rb',
+  '.php',
+  '.swift',
+  '.kt',
+  '.md',
+] as const
+
