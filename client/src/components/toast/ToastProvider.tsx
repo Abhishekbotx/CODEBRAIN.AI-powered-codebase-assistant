afaa226f646d162cc5d179c4506539870833a193
Abhishekbotx <abhishekbotx@gmail.com>
2026-04-17 09:07:30 +0530

    feat(Toast/Provider):Provider initilzation

---
+++ client/src/components/toast/ToastProvider.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from 'react'
+import { useCallback, useMemo, useState } from 'react'
+import { ToastContext } from './toastContext'
+import type { ToastInput, ToastItem } from '../../types'
+import '../../index.css'
+
+export function ToastProvider({ children }: { children: ReactNode }) {
+  return
+}
